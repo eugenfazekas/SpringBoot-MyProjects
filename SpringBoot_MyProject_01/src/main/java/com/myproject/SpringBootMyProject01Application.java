99822a8e540ad16b901a7a49f8c68e05a5e81aec
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-05-19 16:45:29 +0300

    MyProject01 Init(rebuild and update DemoProject)

---
+++ SpringBoot_MyProject_01/src/main/java/com/myproject/SpringBootMyProject01Application.java
@@ -0,0 +1,13 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyProject01Application.class, args);
+	}
+
+}
