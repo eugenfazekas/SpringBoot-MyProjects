f6d7f1d11aa44e35baefebc98215cd19c2ef9186
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-08-31 17:05:49 +0300

    MyProject added certificate server.p12 and additional settings to application-properties

--- SpringBoot_MyProject_01/src/main/java/com/myproject/SpringBootMyProject01Application.java
+++ SpringBoot_MyProject_01/src/main/java/com/myproject/SpringBootMyProject01Application.java
@@ -1,15 +1,63 @@
 package com.myproject;
 
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @EnableConfigurationProperties
+//@PropertySources({
+//    @PropertySource(value = "file:${appConf}", ignoreResourceNotFound = true)
+//})
+
+
 @SpringBootApplication
-public class SpringBootMyProject01Application {
+public class SpringBootMyProject01Application  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMyProject01Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SpringBootMyProject01Application.class);
+    }
 
+	@Bean 
+	public ServletWebServerFactory servletContainer() {
+		
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+				
+			@Override
+			protected void postProcessContext(Context context) {
+			SecurityConstraint securityConstraint = new SecurityConstraint();
+			securityConstraint.setUserConstraint("CONFIDENTIAL");
+			SecurityCollection collection = new SecurityCollection();
+			collection.addPattern("/*");
+			securityConstraint.addCollection(collection);
+			context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		
+		return tomcat;
+	}
+	
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("https");
+		connector.setPort(444);
+		connector.setSecure(true);
+		connector.setRedirectPort(443);
+		return connector;
+	}
 }
