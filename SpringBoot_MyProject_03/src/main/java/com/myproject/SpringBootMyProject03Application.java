d4886b135883c1bf42d2e9663e0c717b15f1038b
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-01-25 17:44:11 +0200

    init MyProject_03

---
+++ SpringBoot_MyProject_03/src/main/java/com/myproject/SpringBootMyProject03Application.java
@@ -0,0 +1,13 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyProject03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyProject03Application.class, args);
+	}
+
+}
